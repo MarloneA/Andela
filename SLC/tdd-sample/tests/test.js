52c228da4759b85c36276fe4c150f8420e97ff4d
Colin Wagaba <ckwagaba@gmail.com>
2017-07-03 21:27:48 +0300

    Adding more test cases

--- SLC/tdd-sample/tests/test.js
+++ SLC/tdd-sample/tests/test.js
@@ -8,12 +8,22 @@ describe("Factorial", function(){
             assert.equal(myApp.computeFactorial(3), 6);
         }
         );
+
+        it("Should return 120 as factorial for 5", function(){
+            assert.equal(myApp.computeFactorial(5), 120);
+        }
+        );
     }
     );
 
     describe("Handle invalid input", function(){
         it("Should return undefined as factorial for -5", function(){
             assert.equal(myApp.computeFactorial(-5), 'undefined');
+        }
+        );
+
+        it("Should return undefined as factorial for 'demo'", function(){
+            assert.equal(myApp.computeFactorial('demo'), 'undefined');
         }
         );
     }
