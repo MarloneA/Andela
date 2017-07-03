8081d5ed2094ccbb531332c93ba403295b267fe3
Colin Wagaba <ckwagaba@gmail.com>
2017-07-03 21:40:36 +0300

    Adding comments

--- SLC/tdd-sample/src/main.js
+++ SLC/tdd-sample/src/main.js
@@ -4,14 +4,15 @@ module.exports = {
 
     /* Find the factorial of an integer */
     computeFactorial: (value) => {
+        //For non-numeric input
         if (typeof(value) != 'number'){
              return 'undefined';
         }
-        
+        //For negative input
         else if (value < 0){
             return 'undefined';
         }
-
+        //For normal input
         else {
             let factorial = 1;
             for (let counter = value; counter >=1; counter--){
