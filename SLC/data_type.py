c28d036e4d7b69f29a9239b83a13272de5654e6e
Colin Wagaba <ckwagaba@gmail.com>
2017-07-04 10:27:20 +0300

    All tests passed

---
+++ SLC/data_type.py
@@ -0,0 +1,23 @@
+def data_type(arg):
+    
+    if type(arg) == int:
+        if arg == 100:
+            return 'equal to 100'
+        elif arg < 100:
+            return 'less than 100'
+        else:
+            return 'more than 100'
+    
+    if type(arg) == str:
+        return len(arg)
+    
+    if type(arg) == bool:
+        return arg
+    
+    if type(arg) == list:
+        if len(arg) < 3:
+            return None
+        else:
+            return len(arg)
+    if arg is None:
+        return 'no value'
