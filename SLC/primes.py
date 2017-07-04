e48d47d1475fbe92890c9c911648602ad008e406
Colin Wagaba <ckwagaba@gmail.com>
2017-07-04 10:27:40 +0300

    Will get back to this later

--- SLC/primes.py
+++ SLC/primes.py
@@ -6,6 +6,6 @@ def primes(n):
         for i in range(3, n + 1): # we test primality from 3 to n
             if n % 2 == 0:
                 continue # eliminate even numbers greater than 2
-            else:
-                # reduce n by checking only odd numbers greater than 2
+            else: # reduce n by checking only odd numbers greater than 2
+                
         return all_primes_in_range
