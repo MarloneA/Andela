ed3df21021e0b3d166af63b326b63e1788a72b18
Colin Wagaba <ckwagaba@gmail.com>
2017-07-04 07:05:26 +0300

    Improving logic. Using 2 as a pivot

--- SLC/primes.py
+++ SLC/primes.py
@@ -1,5 +1,8 @@
 def primes(n):
-    all_primes_in_range = []
-    for i in range(n + 1):
-        #run the primality test here and add only primes to the list to be returned
-    return all_primes_in_range
+    if n < 2:
+        return 'No primes in range'
+    else: # when is 2 or greater
+        all_primes_in_range = [2] # 2 is our first prime
+        for i in range(3, n + 1): # we test primality from 3 to n
+            # run the primality test here and add only primes to the list to be returned
+        return all_primes_in_range
