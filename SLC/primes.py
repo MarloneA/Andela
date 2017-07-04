d907d7901cc7905b8e8539d08a06da088c8abf32
Colin Wagaba <ckwagaba@gmail.com>
2017-07-04 07:15:17 +0300

    eliminate even numbers greater than 2

--- SLC/primes.py
+++ SLC/primes.py
@@ -4,5 +4,8 @@ def primes(n):
     else: # when is 2 or greater
         all_primes_in_range = [2] # 2 is our first prime
         for i in range(3, n + 1): # we test primality from 3 to n
-            # run the primality test here and add only primes to the list to be returned
+            if n % 2 == 0:
+                continue # eliminate even numbers greater than 2
+            else:
+                # reduce n by checking only odd numbers greater than 2
         return all_primes_in_range
