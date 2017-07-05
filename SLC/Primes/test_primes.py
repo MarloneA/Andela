cfa45238401d05d6998aa3c1ccf6416acce8cb27
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 06:58:28 +0300

    switching to the unittest framework

--- SLC/Primes/test_primes.py
+++ SLC/Primes/test_primes.py
@@ -1,8 +1,17 @@
-""" Using the pytest framework """
+""" Using the unittest framework """
 
 # Test cases:
-# integer input
+# integer input. should be a number
 # input greater or equal to 2
 # is_prime (only two factors: 1 and self) - primality
 
-def test_
+from primes import *
+import unittest
+
+class TestPrimes(unittest.TestCase):
+    
+    def test_valid_input():
+        #
+
+if __name__ == '__main__':
+    unittest.main()
