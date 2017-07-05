c5c53206a9491732e8d780e4d859092e65aec870
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 16:07:43 +0300

    Weather is returned. Just enter your city of choice and you're good

--- Bootcamp/HTTP/weather.py
+++ Bootcamp/HTTP/weather.py
@@ -1,4 +1,5 @@
 import urllib
+import json
 """
     Simple Weather API using the open weather data API
     In this app, you'll be entering the city of choice,
@@ -9,5 +10,8 @@ url = 'http://api.openweathermap.org/data/2.5/weather'
 values = {'APPID': '4cdcbe29c9a481e33adf4d520f072685', 'q': city}
 data = urllib.urlencode(values)
 
-json = urllib.urlopen('http://api.openweathermap.org/data/2.5/weather?%s' % data).read()
-print json
+response_string = urllib.urlopen('http://api.openweathermap.org/data/2.5/weather?%s' % data).read()
+json = json.loads(response_string)
+#print response_string
+#print json
+print 'The weather in {} is {}'.format(city, json['weather'][0]['description'])
