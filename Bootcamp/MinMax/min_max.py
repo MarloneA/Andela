61d555b52842de1743ae5232381f4348668aa286
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 16:55:59 +0300

    the wrong sorting function?

--- Bootcamp/MinMax/min_max.py
+++ Bootcamp/MinMax/min_max.py
@@ -1,10 +1,10 @@
 def find_max_min(list_of_input):
     if isinstance(list_of_input, list):
         if len(list_of_input) > 0:
-            sorted_list_of_inputs = sorted(list_of_input)
-            min_max_list = [sorted_list_of_inputs[0], sorted_list_of_inputs[-1]]
-            if sorted_list_of_inputs[0] == sorted_list_of_inputs[-1]:
-                return len(min_max_list)
+            list_of_input.sort()
+            min_max_list = [list_of_input[0], list_of_input[-1]]
+            if list_of_input[0] == list_of_input[-1]:
+                return [len(list_of_input)]
             else:
                 return min_max_list
         else:
