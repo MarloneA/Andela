2219d963efaa793372283dd5e68b379198bd2878
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 04:42:58 +0300

    I think I jus messed this up :/

--- SLC/Primes/primes.py
+++ SLC/Primes/primes.py
@@ -2,7 +2,7 @@ def primes(n):
     if n < 2:
         print 'No primes in range'
     else: # when is 2 or greater
-        all_primes_in_range = [] # initialize an empty list of primes
+        all_primes_in_range = [2] # 2 is our first prime
         for i in range(3, n + 1): # we test primality from 3 to n
             if i % 2 == 0:
                 continue # eliminate even numbers greater than 2
