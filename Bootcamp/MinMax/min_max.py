983e63a3b72afc411f859fdf4ea4a0bf8f3dff7c
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 16:57:06 +0300

    need to read more on python's sorting functions

--- Bootcamp/MinMax/min_max.py
+++ Bootcamp/MinMax/min_max.py
@@ -1,7 +1,7 @@
 def find_max_min(list_of_input):
     if isinstance(list_of_input, list):
         if len(list_of_input) > 0:
-            list_of_input.sort() # read more on sorting
+            list_of_input.sort()
             min_max_list = [list_of_input[0], list_of_input[-1]]
             if list_of_input[0] == list_of_input[-1]:
                 return [len(list_of_input)]
