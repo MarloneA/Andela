ee92f7323a26f2df38f136d084668aeb0b052b8f
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 04:42:34 +0300

    I think I jus messed this up :/

---
+++ SLC/Primes/primes.py
@@ -0,0 +1,17 @@
+def primes(n):
+    if n < 2:
+        print 'No primes in range'
+    else: # when is 2 or greater
+        all_primes_in_range = [] # initialize an empty list of primes
+        for i in range(3, n + 1): # we test primality from 3 to n
+            if i % 2 == 0:
+                continue # eliminate even numbers greater than 2
+            else: # reduce n by checking only odd numbers greater than 2
+                for x in range(2, i):# factorization. is it divisible by any other number less than it?
+                    if i % x == 0:
+                        continue
+                    else:
+                        all_primes_in_range.append(i)
+        print all_primes_in_range
+
+primes(10)
