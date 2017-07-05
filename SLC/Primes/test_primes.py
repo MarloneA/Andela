b59ae5582c5e234dcb757ce63347e7534ff73962
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 07:06:05 +0300

    type test passed

--- SLC/Primes/test_primes.py
+++ SLC/Primes/test_primes.py
@@ -10,8 +10,8 @@ import unittest
 
 class TestPrimes(unittest.TestCase):
     
-    def test_valid_input():
-        #
+    def test_invalid_input(self):
+        self.assertRaises(TypeError, primes, 'string')
 
 if __name__ == '__main__':
     unittest.main()
