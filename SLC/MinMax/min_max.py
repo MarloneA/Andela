bcbca334b919dde5dc6cf9ee412a7796c9e4537c
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 11:56:20 +0300

    still failing one test. will get back later

--- SLC/MinMax/min_max.py
+++ SLC/MinMax/min_max.py
@@ -2,7 +2,11 @@ def find_max_min(list_of_input):
     if isinstance(list_of_input, list):
         if len(list_of_input) > 0:
             sorted_list_of_inputs = sorted(list_of_input)
-            return [sorted_list_of_inputs[0], sorted_list_of_inputs[-1]]
+            min_max_list = [sorted_list_of_inputs[0], sorted_list_of_inputs[-1]]
+            if sorted_list_of_inputs[0] == sorted_list_of_inputs[-1]:
+                return len(min_max_list)
+            else:
+                return min_max_list
         else:
             return False
     else:
