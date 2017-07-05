b8928ca38a7bd8c3ffec7f9561a8759732f5225a
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 12:34:44 +0300

    can't believe I'm failing all the tests :/

---
+++ SLC/word_count.py
@@ -0,0 +1,13 @@
+def words(string_input):
+    if type(string_input) is not str:
+        raise TypeError('Input should be a string')
+    elif not string_input or string_input == ' ':
+        return False
+    else:
+        word_list = string_input.split()
+        count_dict = {}
+        for word in word_list:
+            if word in count_dict:
+                continue
+            else:
+                count_dict[word] = word_list.count(word)
