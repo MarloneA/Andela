ced515fe61f88829efc7e8ace92bd6c7e4d7f236
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 15:38:12 +0300

    data not going with the request

--- Bootcamp/HTTP/weather.py
+++ Bootcamp/HTTP/weather.py
@@ -4,6 +4,10 @@ import urllib
     In this app, you'll be entering the city of choice,
     and you'll be able to know the current weather there :D
 """
-#city = raw_input('Enter your city\n')
-response = urllib.urlopen("http://api.openweathermap.org/data/2.5/weather?q=Kampala&APPID=4cdcbe29c9a481e33adf4d520f072685").read()
-print response
+city = raw_input('Enter your city\n')
+url = 'http://api.openweathermap.org/data/2.5/weather'
+values = {'APPID': '4cdcbe29c9a481e33adf4d520f072685', 'q': city}
+data = urllib.urlencode(values)
+
+json = urllib.urlopen(url, data).read()
+print json
