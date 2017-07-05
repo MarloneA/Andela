243b107bf656edff0cb9277af79422f3f4fdfa24
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 05:04:23 +0300

    clean up

--- SLC/Primes/primes.py
+++ SLC/Primes/primes.py
@@ -4,9 +4,10 @@ def primes(n):
     else:
         all_primes_in_range = []
         for i in range(2, n + 1):
-            for x in range(2, i):# factorization
+            """ factorization """
+            for x in range(2, i):
                 if i % x == 0: # is it divisible by any other number less than it?
                     break
-            else:
+            else: # loop fell through without finding a factor
                 all_primes_in_range.append(i)
         print all_primes_in_range
