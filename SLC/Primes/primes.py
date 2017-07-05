6f9c142b3c31e2fb26a84bf944d3020a76cbbd6b
Colin Wagaba <ckwagaba@gmail.com>
2017-07-05 05:01:43 +0300

    using continue is a bad idea

--- SLC/Primes/primes.py
+++ SLC/Primes/primes.py
@@ -1,17 +1,12 @@
 def primes(n):
     if n < 2:
         print 'No primes in range'
-    else: # when is 2 or greater
-        all_primes_in_range = [2] # 2 is our first prime
-        for i in range(3, n + 1): # we test primality from 3 to n
-            if i % 2 == 0:
-                continue # eliminate even numbers greater than 2
-            else: # reduce n by checking only odd numbers greater than 2
-                for x in range(2, i):# factorization. is it divisible by any other number less than it?
-                    if i % x == 0:
-                        continue
-                    else:
-                        all_primes_in_range.append(i)
+    else:
+        all_primes_in_range = []
+        for i in range(2, n + 1):
+            for x in range(2, i):# factorization
+                if i % x == 0: # is it divisible by any other number less than it?
+                    break
+            else:
+                all_primes_in_range.append(i)
         print all_primes_in_range
-
-primes(10)
