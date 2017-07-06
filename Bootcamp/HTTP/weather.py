83f12a5f867560450f0af2178a96267c89ad2555
Colin Wagaba <ckwagaba@gmail.com>
2017-07-06 04:58:52 +0300

    validating user input

--- Bootcamp/HTTP/weather.py
+++ Bootcamp/HTTP/weather.py
@@ -5,13 +5,18 @@ import json
     In this app, you'll be entering the city of choice,
     and you'll be able to know the current weather in that city :D
 """
-city = raw_input('Enter your city\n')
+def get_city():
+    city = raw_input('Enter your city\n')
+    if city.isdigit():
+        return get_city()
+    else:
+        return city
+
+city = get_city()
 url = 'http://api.openweathermap.org/data/2.5/weather'
 values = {'APPID': '4cdcbe29c9a481e33adf4d520f072685', 'q': city}
 data = urllib.urlencode(values)
 
 response_string = urllib.urlopen('http://api.openweathermap.org/data/2.5/weather?%s' % data).read()
 json = json.loads(response_string)
-#print response_string
-#print json
 print 'The weather in {} is {}'.format(city, json['weather'][0]['description'])
