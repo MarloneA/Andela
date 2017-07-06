b1f2c52afb2d570c95ebd01fe5f50941c28fea49
Colin Wagaba <ckwagaba@gmail.com>
2017-07-06 19:49:49 +0300

    Unhandled Exception: 'count'

--- Bootcamp/Binary/binary_search.py
+++ Bootcamp/Binary/binary_search.py
@@ -15,24 +15,23 @@ class List(object): # creates the list to be searched by the binary search
         return self.created_list
 
 class BinarySearch(List): # searches to FIND A NUMBER WE WANT from the created or returned list using binary search
-    def __init__(self):
-        self.count = 1 # incremented at each iteration
+    def __init__(self, a, b):
+        List.__init__(self, a, b)
     
     def search(self, value_to_find):
-        list_to_search = self.create_list() # returned list. method inherited from List class
-        
+        self.count = 1 # incremented at each iteration
+        self.list_to_search = self.create_list() # returned list. method inherited from List class
         """ Binary Search ALgorithm to find Index of Value """
         # keep in mind the list is sorted by default
         first_number = 0 # these are indicies
-        last_number = len(list_to_search[]) - 1
-        it_is_value = False
+        last_number = len(self.list_to_search) - 1
 
-        while first_number <= last_number and not it_is_value:
+        while first_number <= last_number:
             middle_number = (first_number + last_number) // 2 # find the middle item (dynamic. keeps on changing)
-            if list_to_search[middle_number] == value_to_find:
+            if self.list_to_search[middle_number] == value_to_find:
                 break # value found
             else:
-                if value_to_find < list_to_search[middle_number]:
+                if value_to_find < self.list_to_search[middle_number]:
                     last_number = middle_number - 1
                 else:
                     first_number = middle_number + 1
